e776cdaa76ec028a9e23bdf1bafea040c0ac2bcd
DG Kim <donggyunkim@gmail.com>
2015-04-28 01:21:29 -0400

    Template for 0.1.2 migration

---
+++ dg/calendar/migrations/dev_0_1_2.php
@@ -0,0 +1,43 @@
+<?php
+/**
+*
+* @package phpBB Extension - DG Calendar
+* @copyright (c) 2015 DG Kim
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace dg\calendar\migrations;
+
+class dev_0_1_1 extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array('\dg\calendar\migrations\dev_0_1_1');
+	}
+  
+ 	public function update_data()
+	{
+		return array(
+			array('config.update', array('calendar_dg_version', '0.1.2')),
+		);
+	}
+	
+	public function update_schema()
+	{
+		return array(
+		);
+	}
+  
+  	public function revert_data()
+	{
+		return array(
+		);
+	}
+  
+  	public function revert_schema()
+	{
+		return array(
+		);
+	}
+}
